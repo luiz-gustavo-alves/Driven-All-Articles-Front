294b43bdd291f5cd262e176b4f98539b2878cc5b
Matheus <matheusvmja@gmail.com>
2023-07-16 15:16:53 -0300

    Feat: Checkout post request

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,9 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import styled from "styled-components";
 import AllArticlesLogo from "../../components/AllArticlesLogo";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 export default function Checkout() {
+
+    const [form, setForm] = useState({
+        name: "",
+        address: "",
+        complement: "",
+        zipcode: "",
+        cardname: "",
+        cardnumber: "",
+        expdate: "",
+        seccode: ""
+    })
+    const navigate = useNavigate()
+    const { auth } = useAuth()
+    const config = { headers: { Authorization: `Bearer ${auth.token}` } }
+
+    function handleForm(e) {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    function submitForm(e) {
+        e.preventDefault()
+
+        axios.post(`${import.meta.env.VITE_API_URL}/checkout`, form, config)
+            .then(res => navigate("/checkout-done"))
+            .catch(err => alert(err.response.data))
+    }
 
     return (
         <CheckoutContainer>
@@ -20,21 +48,23 @@ export default function Checkout() {
                     <p>Frete: R$0,00</p>
                     <h1>Total do pedido: </h1> R$0,00
                 </InfoContainer>
-                <AddressContainer>
-                    <h1>Endereço de entrega</h1>
-                    <input required placeholder="Nome" type="text"></input>
-                    <input required placeholder="Endereço" type="text"></input>
-                    <input required placeholder="Complemento" type="number" min="0"></input>
-                    <input required placeholder="CEP" type="number" min="0"></input>
-                </AddressContainer>
-                <PaymentContainer>
-                    <h1>Informações do pagamento</h1>
-                    <input required placeholder="Nome no cartão" type="text"></input>
-                    <input required placeholder="Número no cartão" type="number"></input>
-                    <input required placeholder="Data de expiração" type="month"></input>
-                    <input required placeholder="Código de segurança (CVV)" type="number" min="0"></input>
-                </PaymentContainer>
-                <button>Confirmar Pedido</button>
+                <form onSubmit={submitForm}>
+                    <AddressContainer>
+                        <h1>Endereço de entrega</h1>
+                        <input required placeholder="Nome" name="name" value={form.name} onChange={handleForm}></input>
+                        <input required placeholder="Endereço" name="address" value={form.address} onChange={handleForm}></input>
+                        <input required placeholder="Complemento" type="tel" maxLength={4} name="complement" value={form.complement} onChange={handleForm}></input>
+                        <input required placeholder="CEP" type="tel" maxLength={8} name="zipcode" value={form.zipcode} onChange={handleForm}></input>
+                    </AddressContainer>
+                    <PaymentContainer>
+                        <h1>Informações do pagamento</h1>
+                        <input required placeholder="Nome no cartão" name="cardname" value={form.cardname} onChange={handleForm}></input>
+                        <input required placeholder="Número no cartão" type="tel" maxLength={19} name="cardnumber" value={form.cardnumber} onChange={handleForm}></input>
+                        <input required placeholder="Data de expiração (MM/AAAA)" type="tel" maxLength={7} name="expdate" value={form.expdate} onChange={handleForm}></input>
+                        <input required placeholder="Código de segurança (CVV)" type="tel" maxLength={4} name="seccode" value={form.seccode} onChange={handleForm}></input>
+                    </PaymentContainer>
+                    <button type="submit">Confirmar Pedido</button>
+                </form>
             </CheckoutItems>
 
         </CheckoutContainer>
