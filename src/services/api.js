ac188d22acc196c8e89bd8dcb2d423f3943c11a6
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-17 16:38:38 -0300

    chore: using API url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL =  `${import.meta.env.VITE_API_URL}`;
 
 const createConfig = (token) => {
     return {headers: {Authorization: `Bearer ${token}`}};
